3a5ea18f8252a56f0158038cd637a6f0fb2b7bb9
Nahoris <albertjedrych@gmail.com>
2022-06-04 20:43:12 +0200

    Zad 9e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        int num1 = 0, num2 = 0;
+        do {
 
-       do {
-           int num1=0, num2=0;
-           System.out.print("Podaj pierwsza liczbe: ");
-           num1 = input.nextInt();
-           if (num1 != 0) {
-               System.out.print("Podaj druga liczbe: ");
-               num2 = input.nextInt();
-           }
-           System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
-       }while ((num1 != 0) && (num2 != 0));
+            System.out.print("Podaj pierwsza liczbe: ");
+            num1 = input.nextInt();
+            num2 = input.nextInt();
+            if ((num1 == 0) || (num2 == 0)) break;
+            System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
 
+        }
+        while (true) ;
     }
 }
+
