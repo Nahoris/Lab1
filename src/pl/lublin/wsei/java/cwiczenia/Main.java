a9e36f91cf225e8f86db41dfc1c46a39fd90517f
Nahoris <albertjedrych@gmail.com>
2022-06-04 20:31:55 +0200

    Zad 9B

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -7,11 +7,15 @@ public class Main {
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
        int num1, num2;
-       System.out.print("Podaj pierwsza liczbe: ");
-       num1 = input.nextInt();
-       System.out.print("Podaj druga liczbe: ");
-       num2 = input.nextInt();
-       System.out.printf("Wynik dodawania %d +%d = %d%n", num1,num2,num1+num2);
+       do {
+           System.out.print("Podaj pierwsza liczbe: ");
+           num1 = input.nextInt();
+           if (num1 != 0) {
+               System.out.print("Podaj druga liczbe: ");
+               num2 = input.nextInt();
+           }
+           System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
+       }while ((num1 != 0) && (num2 != 0));
 
     }
 }
