86f495243631f660176ffbd32c244c669d545df2
Nahoris <albertjedrych@gmail.com>
2022-06-04 20:23:19 +0200

    Zad 8

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,11 +1,17 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-       int a = 3;
-       double b = 4.21;
-       String s = "jakis tekst";
-       System.out.printf("a = %d, b = %f, s = %s %n",a,b,s);
+       Scanner input = new Scanner(System.in);
+       int num1, num2;
+       System.out.print("Podaj pierwsza liczbe: ");
+       num1 = input.nextInt();
+       System.out.print("Podaj druga liczbe: ");
+       num2 = input.nextInt();
+       System.out.printf("Wynik dodawania %d +%d = %d%n", num1,num2,num1+num2);
+
     }
 }
