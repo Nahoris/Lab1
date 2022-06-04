489ed3ef978e123e6b3e2c5f1c59d09e39cf6179
Nahoris <albertjedrych@gmail.com>
2022-06-04 19:57:59 +0200

    Zad 6

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,7 +2,7 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static voiid main(String[] args) {
         System.out.println("Hello java world\n");
     }
 }
